3f0314788cb75c19b8953ce69aca43e4c582cc25
kimmincheo <106697942+kimmincheo@users.noreply.github.com>
2023-04-14 07:00:10 +0900

    Add files via upload

--- backtest.py
+++ backtest.py
@@ -193,7 +193,7 @@ while True:
                 continue
         
         #매수 부분
-        if get_dispersion() >= 360000:
+        if get_dispersion() >= 100000:
             if get_target_value(KrCoin[max]) >= 20000000000: #거래대금이 20,000백만 이상인가
                     
                     while True:
@@ -209,14 +209,14 @@ while True:
                     
                     #골든크로스 매수 
                     if ma5.iloc[-1] > ma5.iloc[-2]  and ma5.iloc[-1] > ma60.iloc[-1] and (ma5.iloc[-1]-ma60.iloc[-1])/ma5.iloc[-1]*100 <= 0.7:#골든 크로스 매수   
-                        upbit.buy_market_order(KrCoin[max],360000)
+                        upbit.buy_market_order(KrCoin[max],100000)
                         print("시간 : %s %s 골든크로스 매수" %(now,KrCoin[max]))
                         time.sleep(1)
                         get_target_sell(KrCoin[max])
 
-                    if (ma5.iloc[-1]-ma60.iloc[-1])/ma5.iloc[-1]*100 <=-2:
+                    if (ma5.iloc[-1]-ma60.iloc[-1])/ma5.iloc[-1]*100 <=-1.7:
 
-                        upbit.buy_market_order(KrCoin[max],440000)
+                        upbit.buy_market_order(KrCoin[max],100000)
                         print("시간 : %s %s 골든크로스 매수" %(now,KrCoin[max]))
                         time.sleep(1)
                         get_target_sell(KrCoin[max])
