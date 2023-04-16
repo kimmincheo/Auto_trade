ff6d8ba9bbcfa6472d4ef66892e1af574ff09606
kimmincheo <106697942+kimmincheo@users.noreply.github.com>
2023-04-16 18:07:14 +0900

    Add files via upload

--- backtest.py
+++ backtest.py
@@ -165,14 +165,14 @@ while True:
                         except Exception as e:
                             continue
                     
-                    # for ca in cancel:
+                    for ca in cancel:
                         
-                    #     if (ma60.iloc[-1] == ma5.iloc[-1]) and ma5[-2] > ma5[-1] and ma5[-3] > ma5[-2]:
-                    #         upbit.cancel_order(ca['uuid'])
-                    #         time.sleep(0.2)
-                    #         balanc = upbit.get_balance(ticker)
-                    #         upbit.sell_market_order("KRW-%s" %ticker, balanc)
-                    #         print("KRW-%s 데드크로스 매도" %ticker)
+                        if (ma60.iloc[-1] == ma5.iloc[-1]) and ma5[-2] > ma5[-1] and ma5[-3] > ma5[-2]:
+                            upbit.cancel_order(ca['uuid'])
+                            time.sleep(0.2)
+                            balanc = upbit.get_balance(ticker)
+                            upbit.sell_market_order("KRW-%s" %ticker, balanc)
+                            print("KRW-%s 데드크로스 매도" %ticker)
 
                     if price_avg <= -2:
                         for ca in cancel:
@@ -208,7 +208,7 @@ while True:
                     
                     
                     #골든크로스 매수 
-                    if ma5.iloc[-1] > ma5.iloc[-2]  and (ma5.iloc[-1]-ma60.iloc[-1])/ma5.iloc[-1]*100 <= 0.32 and (ma5.iloc[-1]-ma60.iloc[-1])/ma5.iloc[-1]*100 >= 0:#골든 크로스 매수   
+                    if ma5.iloc[-1] > ma5.iloc[-2]  and ma5.iloc[-2] > ma5.iloc[-3]  and ma5.iloc[-3] > ma5.iloc[-4]  and (ma5.iloc[-1]-ma60.iloc[-1])/ma5.iloc[-1]*100 <= 0.32 and (ma5.iloc[-1]-ma60.iloc[-1])/ma5.iloc[-1]*100 >= 0:#골든 크로스 매수   
                         upbit.buy_market_order(KrCoin[max],100000)
                         print("시간 : %s %s 골든크로스 매수" %(now,KrCoin[max]))
                         time.sleep(1)
